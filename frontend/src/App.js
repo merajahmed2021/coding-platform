5d40c16c222b889163a54e11dae37c6e164ca886
Meraj.Ahmed <merajahmed2112@gmail.com>
2021-09-23 13:35:50 +0530

    logo deleted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,9 +25,7 @@ export default function App() {
   showPrintMargin={true}
   showGutter={true}
   highlightActiveLine={true}
-//   value={`function onLoad(editor) {
-//   console.log("i've loaded");
-// }`}
+
   setOptions={{
   enableBasicAutocompletion: true,
   enableLiveAutocompletion: true,
